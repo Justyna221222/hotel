2853186d7aa1d850a4ad7ebcc8e874aa3b16e956
Justyna <justynaskurzok@gmail.com>
2024-04-08 09:09:35 +0200

    formated code

--- Pulpit/Hotele/search_hotel/src/components/Form.tsx
+++ Pulpit/Hotele/search_hotel/src/components/Form.tsx
@@ -16,10 +16,7 @@ interface IFormInput {
 }
 
 export const Form: React.FC = () => {
-  const {
-    register,
-    handleSubmit,
-  } = useForm<IFormInput>();
+  const { register, handleSubmit } = useForm<IFormInput>();
 
   const onSubmit = (data: IFormInput) => {
     console.log(data);
@@ -27,44 +24,46 @@ export const Form: React.FC = () => {
 
   return (
     <div>
-    <div><h2>Insert hotel into database</h2></div>
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label>Name</label>
-      <input {...register("name")} />
-
-      <label>Address</label>
-      <input {...register("street")} />
-
-      <div className="input-group">
-        <div className="input-field">
-          <label>City</label>
-          <input {...register("city")} />
-        </div>
-        <div className="input-field">
-          <label>State</label>
-          <input {...register("state")} />
-        </div>
-        <div className="input-field">
-          <label>ZIP Code</label>
-          <input {...register("zipcode")} />
-        </div>
+      <div>
+        <h2>Insert hotel into database</h2>
       </div>
-      <label>Phone</label>
-      <input {...register("phone")} />
-      <label>Category</label>
-      <select {...register("category")}>
-        <option value="4*">4*</option>
-        <option value="5*">5*</option>
-      </select>
-      <label>Email</label>
-      <input {...register("mail")} />
-      <label>Website</label>
-      <input {...register("website")} />
-      <label>Logo</label>
-      <input {...register("logo")} type="file"/>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>Name</label>
+        <input {...register("name")} />
 
-      <input type="submit" />
-    </form>
+        <label>Address</label>
+        <input {...register("street")} />
+
+        <div className="input-group">
+          <div className="input-field">
+            <label>City</label>
+            <input {...register("city")} />
+          </div>
+          <div className="input-field">
+            <label>State</label>
+            <input {...register("state")} />
+          </div>
+          <div className="input-field">
+            <label>ZIP Code</label>
+            <input {...register("zipcode")} />
+          </div>
+        </div>
+        <label>Phone</label>
+        <input {...register("phone")} />
+        <label>Category</label>
+        <select {...register("category")}>
+          <option value="4*">4*</option>
+          <option value="5*">5*</option>
+        </select>
+        <label>Email</label>
+        <input {...register("mail")} />
+        <label>Website</label>
+        <input {...register("website")} />
+        <label>Logo</label>
+        <input {...register("logo")} type="file" />
+
+        <input type="submit" />
+      </form>
     </div>
   );
 };
