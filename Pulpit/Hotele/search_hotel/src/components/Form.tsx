b84cd2bca7442c7390b0c1cd47819649652ab7bf
Justyna <justynaskurzok@gmail.com>
2024-04-22 10:35:42 +0200

    Form add input upload more photos

--- Pulpit/Hotele/search_hotel/src/components/Form.tsx
+++ Pulpit/Hotele/search_hotel/src/components/Form.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import {useState} from 'react';
 import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 import "./Form.css";
+
+const MAX_COUNT = 5;
 
 interface IFormInput {
   name: string;
@@ -13,15 +17,35 @@ interface IFormInput {
   mail: string;
   website: string;
   logo: File;
+  photos: File;
+  file: File;
 }
 
 export const Form: React.FC = () => {
-  const { register, handleSubmit } = useForm<IFormInput>();
-
+  const { register, control, handleSubmit } = useForm<IFormInput>();
+  const [uploadedFiles, setUploadedFiles] = useState<File[]>();
+  const [showPhotoList, setPhotoList] = useState(false);
   const onSubmit = (data: IFormInput) => {
     console.log(data);
   };
 
+  const handleUploadFiles = function(photoFiles:any[]) {
+    let uploaded:File[] = [];
+    photoFiles.some((photograph:File) => {
+      uploaded!.push(photograph);
+        if (uploaded != null && uploaded.length <= MAX_COUNT) {
+          setUploadedFiles(uploaded);
+          setPhotoList(true);
+        } else {
+          alert("Możesz wprowadzić maksymalnie 5 zjęć");
+          setUploadedFiles([]);
+        }
+    })
+  }
+  const onChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const chosenFiles = Array.prototype.slice.call(e.target.files);
+    handleUploadFiles(chosenFiles);
+};
   return (
     <div>
       <div>
@@ -61,9 +85,25 @@ export const Form: React.FC = () => {
         <input {...register("website")} />
         <label>Logo</label>
         <input {...register("logo")} type="file" />
-
+        <label htmlFor='fileUpload'>
+          Zdjęcia
+        </label>
+        <p>&#40; Możesz wybrać do 5 zdjęć. &#41;</p>
+        <input {...register("photos")} type="file" id='fileUpload' multiple
+        accept='application/pdf, image/png' onChange={onChange}/>
+                {showPhotoList && (
+      <React.Fragment>
+      <div className="uploaded-files-list">
+          {uploadedFiles!.map(file => (
+            <div key={file.lastModified}>
+                {file.name}
+            </div>
+        ))}
+      </div>
+      </React.Fragment>)}
         <input type="submit" />
       </form>
+      <DevTool control={control} />
     </div>
   );
 };
